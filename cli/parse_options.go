135b074f7f2797375087a8841432c31401c6170e
hexstasy <isasuperhack@gmail.com>
2025-06-10 08:13:46 +0300

    update: up ui errors and display version

--- cli/parse_options.go
+++ cli/parse_options.go
@@ -4,13 +4,13 @@ import (
   "flag"
   "fmt"
   "github.com/hexstasy/blackroot/config"
+  "github.com/fatih/color"
 )
 
 func ParseOpts(cfg *config.Config) {
   display_version := flag.Bool("version", false, "Display version and exit.")
   flag.Parse();
-
   if *display_version {
-    fmt.Printf("Blackroot version: %s\n", cfg.Settings.Version) 
+    fmt.Printf("Blackroot version: %s\n", color.GreenString(cfg.Settings.Version)) 
   }
 }
--- proc_err/proc_err.go
+++ proc_err/proc_err.go
@@ -3,10 +3,11 @@ package procerr
 import (
   "fmt"
   "os"
+  "github.com/fatih/color"
 )
 
 func Err(msg string) {
-  err_msg := fmt.Sprintf("blackroot error: %s\n", msg) 
+  err_msg := fmt.Sprintf("blackroot %s: %s\n", color.RedString("error"), msg) 
   os.Stderr.WriteString(err_msg)
   os.Exit(1)
 }
